79c2a215dab6af3aa58a5da9064ede874e2d1d6c
Filip Malczak <filip.malczak@gmail.com>
2024-09-04 23:14:36 +0200

    Remove venv from install_develop.sh

--- install_develop.sh
+++ install_develop.sh
@@ -2,9 +2,9 @@
 #This script will install requirements from thinking family from develop branch on GitHub if they aren't installed yet.
 # Should work with bash too, shebang is aligned with my machine (running Linux Mint).
 # Used mainly in CI.
+# YOU MUST ACTIVATE VENV ON YOUR OWN IF YOU USE THIS LOCALLY.
 set -e
 
-source ./venv/bin/activate
 for r in $(cat ./thinking-dependencies.txt)
 do
   echo "Installing $r from develop branch"
